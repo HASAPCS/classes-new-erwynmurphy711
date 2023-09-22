ada5daa2900ae64bbc1f9b4ff3b2c812b2fc5863
erwynmurphy711 <erwynmurphy711@gmail.com>
2023-09-22 10:30:35 +0800

    hello

--- src/main/java/com/example/Clock.java
+++ src/main/java/com/example/Clock.java
@@ -5,16 +5,33 @@ public class Clock {
     private int minutes;
 
     public Clock(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
         // TODO: Initialize the hours and minutes
     }
 
     public void addMinutes(int minutes) {
-        // TODO: Add the value of the minutes parameter to the current minutes. 
-        // Adjust the hours and minutes accordingly to represent a valid time.
-    }
+        this.minutes += minutes;
+        if(this.minutes >= 60){
+            this.minutes = this.minutes - 60;
+            this.hours += 1;
+        }
+        }
+        
 
     public String getTime() {
+        String stringvalue = " ";
+        if(this.hours >= 12){
+            this.hours = this.hours - 12;
+            if(this.hours >= 10){
+                return this.hours+":"+this.minutes;
+            }
+            stringvalue = Integer.toString(this.hours);
+            stringvalue = "0"+stringvalue;
+            return stringvalue+":"+this.minutes;
+        }
+
         // TODO: Return the time in the format "HH:MM"
-        return "";  // Change this default return statement
+        return this.hours + ":" + this.minutes; // Change this default return statement
     }
 }
--- src/main/java/com/example/Rectangle.java
+++ src/main/java/com/example/Rectangle.java
@@ -5,16 +5,22 @@ public class Rectangle {
     private double width;
 
     public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
         // TODO: Initialize the length and width
     }
 
     public double area() {
+        double areaa = 0;
+        areaa = this.width*this.length;
         // TODO: Calculate and return the area of the rectangle
-        return 0;  // Change this default return statement
+        return areaa;  // Change this default return statement
     }
 
     public double perimeter() {
+        double perimeterr = 0;
+        perimeterr = 2*this.length + 2*this.width;
         // TODO: Calculate and return the perimeter of the rectangle
-        return 0;  // Change this default return statement
+        return perimeterr;  // Change this default return statement
     }
 }
